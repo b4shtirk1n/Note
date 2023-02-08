ab5531a89e17727a66562a248ec82fa920780afc
b4shtirk1n <wowan4512@gmail.com>
2023-02-08 17:16:01 +0500

    Make layout & test file AddRead

--- Note/MainWindow.xaml.cs
+++ Note/MainWindow.xaml.cs
@@ -1,12 +1,29 @@
-﻿using System.Windows;
+﻿using Note.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
 
 namespace Note
 {
     public partial class MainWindow : Window
     {
+        List<NoteItem> items = new List<NoteItem>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            string path = "Documents";
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string file in files)
+            {
+                items.Add(new NoteItem(file));
+            }
+
+            MessageBox.Show($"{items.First().Name} {items.First().Description}");
         }
     }
 }
+
---
+++ Note/Models/NoteItem.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace Note.Models
+{
+    public class NoteItem
+    {
+        public string Name { get; private set; }
+
+        public string Description { get; private set; }
+
+        public NoteItem(string path)
+        {
+            Name = new FileInfo(path).Name;
+            Description = File.ReadAllText(path);
+        }
+    }
+}
+
