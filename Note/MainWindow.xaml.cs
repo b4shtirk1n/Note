6a9ff3b9c9a7dbbc2e44131ee59eb7c861031908
b4shtirk1n <wowan4512@gmail.com>
2023-02-10 17:31:52 +0500

    make editor & layout

--- Note/MainWindow.xaml.cs
+++ Note/MainWindow.xaml.cs
@@ -1,18 +1,26 @@
 ﻿using Note.Models;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
 
 namespace Note
 {
     public partial class MainWindow : Window
     {
         private readonly List<NoteItem> items = new List<NoteItem>();
+        private NoteItem currentItem;
 
         public MainWindow()
         {
             InitializeComponent();
+            Update();
+        }
+
+        private void Update()
+        {
+            items.Clear();
 
             string path = "Documents";
             string[] files = Directory.GetFiles(path);
@@ -21,8 +29,32 @@ namespace Note
             {
                 items.Add(new NoteItem(file));
             }
-
             NoteList.ItemsSource = items;
+        }
+
+        private void IsEdit(TextRange textRange)
+        {
+            if (textRange.Text != "\r\n" &&
+                textRange.Text.Substring(0, currentItem.Length) != currentItem.GetText())
+            {
+                MessageBox.Show("");
+            }
+        }
+
+        private void NoteListSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TextRange textRange = new TextRange(Editor.Document.ContentStart,
+                Editor.Document.ContentEnd);
+
+            IsEdit(textRange);
+
+            currentItem = (NoteItem)NoteList.SelectedItem;
+            textRange.Text = currentItem.GetText();
+        }
+
+        private void ExitClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
     }
 }
--- Note/Models/NoteItem.cs
+++ Note/Models/NoteItem.cs
@@ -4,17 +4,25 @@ namespace Note.Models
 {
     public class NoteItem
     {
+        public string Path { get; private set; }
+
         public string Name { get; private set; }
+
+        public int Length { get; private set; }
 
         public string Description { get; private set; }
 
-        public string Path { get; private set; }
-
         public NoteItem(string path)
         {
-            Name = new FileInfo(path).Name.Replace(".txt", "");
-            Description = File.ReadAllText(path);
             Path = path;
+            Name = new FileInfo(path).Name.Replace(".txt", "");
+            Length = GetText().Length;
+            Description = Length > 80 ? GetText().Substring(0, 80) : GetText();
+        }
+
+        public string GetText()
+        {
+            return File.ReadAllText(Path);
         }
     }
 }
